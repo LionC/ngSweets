fb626747022cc2ae03e51c59e8e7305f5efda036
LionC <lionc-ilu@hotmail.de>
2015-08-29 20:21:28 +0200

    new gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-gulp.task('build', function(){
-  return gulp.src(["src/ngSweetsModule.js", "src/**/*.js"])
+gulp.task('copy-packagefiles', function(){
+  return gulp.src(['bower.json'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src(['src/ngSweetsModule.js', 'src/**/*.js'])
   .pipe(concat('ngsweets.js'))
   .pipe(uglify())
-  .pipe(gulp.dest("dist"));
+  .pipe(gulp.dest('dist'));
 });
+
+gulp.task('build', ['scripts', 'copy-packagefiles']);
 
 gulp.task('watch', function(){
   gulp.watch('src/**/*.js', ['build']);
