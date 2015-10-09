88ee94bb9aad12b0a0760ef785f7c9de65a16230
LionC <lionc-ilu@hotmail.de>
2015-10-09 21:59:51 +0200

    current build

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,18 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-gulp.task('scripts', function(){
-  return gulp.src(['src/ngSweetsModule.js', 'src/**/*.js'])
+gulp.task('build', function(){
+  return gulp.src(["src/ngSweetsModule.js", "src/**/*.js"])
   .pipe(concat('ngsweets.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest("dist"));
 });
 
 gulp.task('debug', function(){
-  return gulp.src(['src/ngSweetsModule.js', 'src/**/*.js'])
+  return gulp.src(["src/ngSweetsModule.js", "src/**/*.js"])
   .pipe(concat('ngsweets.js'))
-  .pipe(gulp.dest('debug'));
+  .pipe(gulp.dest("dist"));
 });
-
-gulp.task('build', ['scripts']);
 
 gulp.task('watch', function(){
   gulp.watch('src/**/*.js', ['build']);
